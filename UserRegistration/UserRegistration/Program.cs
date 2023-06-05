dc038febed619cef6a5776d7b2e1289b627725dd
PREETI2096 <preetirg96@gmail.com>
2023-06-05 14:37:29 +0530

    Added: validation of Firstname

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            UserValidation userValidation = new UserValidation();
+            userValidation.ValidateFirstName();
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/UserValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class UserValidation
+    {
+        public void ValidateFirstName()
+        {
+            string FirstNamePattern = @"^[A-Z][a-z]{2,}$";
+            Regex regexObject = new Regex(FirstNamePattern);
+            Console.WriteLine("Enter First Name: ");
+            string  firstName = Console.ReadLine();
+            if (regexObject.IsMatch(firstName))
+                Console.WriteLine("First name is valid");
+            else
+                Console.WriteLine("Invalid FirstName");
+        }
+    }
+}
